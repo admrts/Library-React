ed215f072bd912379824645594f60dae87a55b0e
admrts <alidemirtas.7@icloud.com>
2023-03-12 19:55:25 +0300

    add toaster package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import { Navbar, Welcome, LoginPage, SignupPage } from "./components";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element={<Welcome />} />
