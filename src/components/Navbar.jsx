b12a0434f3ded17f44b9ddfbc19b6b8a641546e3
admrts <alidemirtas.7@icloud.com>
2023-03-12 20:49:38 +0300

    navbar  > logout button add & profil label add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Box, Container, Stack, Button } from "@mui/material";
+import { Box, Container, Stack, Button, Typography } from "@mui/material";
 import Logo from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import PersonIcon from "@mui/icons-material/Person";
+import { logout as logoutHandle } from "../redux/authSlice";
+import { logout } from "../firebase";
 
 function Navbar() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
   const loginClick = () => {
     navigate("login");
@@ -13,6 +19,10 @@ function Navbar() {
   };
   const logoClick = () => {
     navigate("/");
+  };
+  const logoutClick = async () => {
+    await logout();
+    dispatch(logoutHandle());
   };
   return (
     <Container sx={{ py: 2, borderBottom: "1px solid lightgray" }}>
@@ -29,14 +39,38 @@ function Navbar() {
             onClick={logoClick}
           />
         </Box>
-        <Box sx={{ display: "flex", gap: "5px" }}>
-          <Button variant="outlined" onClick={loginClick}>
-            Log In
-          </Button>
-          <Button variant="contained" onClick={signupClick}>
-            Sign Up
-          </Button>
-        </Box>
+        {user ? (
+          <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+            <Typography sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+              Hello!
+              <Box
+                component="span"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  cursor: "pointer",
+                  "&:hover": {
+                    opacity: [0.6],
+                  },
+                }}
+              >
+                <PersonIcon /> {user.displayName ?? user.email}
+              </Box>
+            </Typography>
+            <Button variant="contained" color="error" onClick={logoutClick}>
+              Log Out
+            </Button>
+          </Box>
+        ) : (
+          <Box sx={{ display: "flex", gap: "5px" }}>
+            <Button variant="outlined" onClick={loginClick}>
+              Log In
+            </Button>
+            <Button variant="contained" onClick={signupClick}>
+              Sign Up
+            </Button>
+          </Box>
+        )}
       </Stack>
     </Container>
   );
