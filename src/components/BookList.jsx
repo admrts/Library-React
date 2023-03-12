2384ea763d3931f5dd64c181e2ac3812650c0c96
admrts <alidemirtas.7@icloud.com>
2023-03-13 01:17:31 +0300

    Booklist design template

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,21 +1,36 @@
-import { Container, Typography, Stack } from "@mui/material";
+import { Container, Typography, Button, Paper } from "@mui/material";
 import React from "react";
 import { ListHeader } from "./";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 function BookList() {
+  const handleClick = () => {
+    console.log("hello world");
+  };
   return (
     <Container>
       <ListHeader />
-      <Stack
-        flexDirection="row"
-        justifyContent="space-around"
-        sx={{ my: 2, p: 1, boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}
+      <Paper
+        onClick={handleClick}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          my: 2,
+          p: 1,
+          cursor: "pointer",
+          borderRadius: "10px",
+          ":hover": {
+            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          },
+        }}
       >
-        <Typography>ASD</Typography>
-        <Typography>ASD</Typography>
-
-        <Typography>ASD</Typography>
-      </Stack>
+        <Typography>Book Name</Typography>
+        <Button onClick={handleClick}>
+          <ArrowForwardIcon />
+        </Button>
+      </Paper>
     </Container>
   );
 }
