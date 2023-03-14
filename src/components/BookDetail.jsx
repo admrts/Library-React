804057d69ad859b4bc56ee979a177f3c8a79198f
admrts <alidemirtas.7@icloud.com>
2023-03-14 19:12:50 +0300

    delete & update buttons

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,4 +1,4 @@
-import { Container, Stack, Typography } from "@mui/material";
+import { Container, Stack, Typography, Button, Box } from "@mui/material";
 import React, { useEffect } from "react";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import { useNavigate } from "react-router-dom";
@@ -18,15 +18,30 @@ function BookDetail() {
   useEffect(() => {}, [dispatch]);
   return (
     <Container>
-      <Stack flexDirection="row" alignItems="center" gap={1} mt={1}>
-        <ArrowCircleLeftIcon onClick={handleClick} sx={{ cursor: "pointer" }} />
-        <Typography
-          variant="h6"
-          onClick={handleClick}
-          sx={{ cursor: "pointer" }}
-        >
-          Book List
-        </Typography>
+      <Stack
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        gap={1}
+        mt={1}
+      >
+        <Stack flexDirection="row" alignItems="center" gap={2}>
+          <ArrowCircleLeftIcon
+            onClick={handleClick}
+            sx={{ cursor: "pointer" }}
+          />
+          <Typography
+            variant="h6"
+            onClick={handleClick}
+            sx={{ cursor: "pointer" }}
+          >
+            Book List
+          </Typography>
+        </Stack>
+        <Stack flexDirection="row" alignItems="center">
+          <Button color="error">Delete</Button>
+          <Button color="success">Update</Button>
+        </Stack>
       </Stack>
       <Stack flexDirection="row" justifyContent="center" mt={3}>
         <Stack alignItems="center">
