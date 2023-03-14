68984a66cc12018b66617a550d73ce5374e45f96
admrts <alidemirtas.7@icloud.com>
2023-03-15 01:00:00 +0300

    welcome screen done

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,23 @@ import {
   Addbook,
   BookDetail,
   Footer,
+  Welcome,
 } from "./components";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { user } = useSelector((store) => store.auth);
   return (
     <div>
       <Toaster />
       <Navbar />
       <Routes>
-        <Route path="/" element={<BookList />} />
+        {user ? (
+          <Route path="/" element={<BookList />} />
+        ) : (
+          <Route path="/" element={<Welcome />} />
+        )}
+
         <Route path="login" element={<LoginPage />} />
         <Route path="signup" element={<SignupPage />} />
         <Route path="addbook" element={<Addbook />} />
